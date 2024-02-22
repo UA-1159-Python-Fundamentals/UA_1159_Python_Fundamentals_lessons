3f42f6aee98defc144f0a64578164f99308401f7
Kateryna Manokha <katerina.manokha@gmail.com>
2024-02-22 20:24:53 +0200

    hi

--- hw/hw02/task_zen.py
+++ hw/hw02/task_zen.py
@@ -6,7 +6,7 @@
 # 6) Sparse is better than dense.
 # 7) Readability counts.
 # 8) Special cases aren't special enough to break the rules.
-# 9) KaterynaManokha
+# 9) Although practicality beats purity.
 # 10) Errors should never pass silently.
 # 11) Although practicality beats purity.
 # 12) ohotass
