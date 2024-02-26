4143a4c40b79148feb1fd391ffdb12208c1e70df
OleksandrArtemenko <silentmoment2014@gmail.com>
2024-02-26 23:18:10 +0200

    added screenshot and phrase for tasks of hw2

--- hw/hw02/task_zen.py
+++ hw/hw02/task_zen.py
@@ -10,7 +10,7 @@
 # 10) Errors should never pass silently.
 # 11) Although practicality beats purity.
 # 12) ohotass
-# 13) OleksandrArtemenko
+# 13) Unless explicitly silenced.
 # 14) In the face of ambiguity, refuse the temptation to guess.
 # 15) Paoenterprizes
 # 16) sherlock440
