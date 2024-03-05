cb0383e0b57ff47138296a640fd201dfe7538c5f
sherlock440 <soroka2179@gmail.com>
2024-03-05 13:29:44 +0200

    Uploaded the 6th homework

---
+++ hw/hw06/sherlock440/Task2.py
@@ -0,0 +1,8 @@
+login = input("What's up, doc? Write your fancy login right here: ")
+
+while login != "First":
+    print("How have you dared to try to get the access having no permissions, sordid spy? Try again while "
+          "our security is coming for your ass")
+    login = input("\nTry again, push your luck: ")
+else:
+    print("Greetings, my dear user!")
