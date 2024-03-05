935cd85abdf51e7113a2994ac67f90661650eab3
Petro Yakubovsky <petroyakubovkii@gmail.com>
2024-03-05 21:13:53 +0200

    add_hw03_task03

--- hw/hw03/yakubovskii/task03.py
+++ hw/hw03/yakubovskii/task03.py
@@ -1,16 +1,9 @@
-num = int(input("Write a four-digit natural number:"))
-num = list
+a = int(input("Write any number for x:"))
+b = int(input("Write any number for y:"))
 
-product = 1
+a, b = b, a
+
+print(f"Interchange the values: x = {a}, y = {b}")
 
 
-if num < 9999 or num > 1000:
-    print(f"The product of digits is:{product}")
-    print(f"Number in reverse order is:", num[::-1])
-    print(f"Sorted {num} is: {sorted(num)}")
-for digit in str(num):
-    product *= int(digit)
-else:
-    print(f"\t Wrong {num}. Try again.")
- 
 
