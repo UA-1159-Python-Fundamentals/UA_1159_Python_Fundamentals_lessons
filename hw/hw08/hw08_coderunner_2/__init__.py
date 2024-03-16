b7363e6278728a0b4fa505bf30d9c371cb0a54b6
gawanyn <shcherbantaras07@gmail.com>
2024-03-17 00:19:14 +0200

    added hw08_coderunner

---
+++ hw/hw08/hw08_coderunner_2/__init__.py
@@ -0,0 +1,1 @@
+from module import print_water_state, BOILING_POINT, FREEZING_POINT
---
+++ hw/hw08/hw08_coderunner_2/module.py
@@ -0,0 +1,12 @@
+FREEZING_POINT = None
+BOILING_POINT = None
+
+def print_water_state(temperature):
+      if temperature <= FREEZING_POINT:
+             print("Solid")
+      elif FREEZING_POINT < temperature < BOILING_POINT:
+             print("Liquid")
+      else:
+             print("Gas")
+
+__all__ = ['print_water_state', 'BOILING_POINT', 'FREEZING_POINT']
