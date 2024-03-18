8fcd0f8cb1aa2f918dd11bee783e5715d53b8685
Bohdan Forkutsa <bogdan200085@gmail.com>
2024-03-19 00:37:44 +0200

    completed task1

--- hw/hw07/BohdanForkutsa/Task_01.py
+++ hw/hw07/BohdanForkutsa/Task_01.py
@@ -1,1 +1,12 @@
 
+def large_number(num1, num2):
+    if num1 > num2:
+        return num1
+    elif num2 > num1:
+        return num2
+    else:
+        return print("num1 = num2")
+
+print(large_number(5, 5))
+print(large_number(6, 5))
+print(large_number(5, 6))
