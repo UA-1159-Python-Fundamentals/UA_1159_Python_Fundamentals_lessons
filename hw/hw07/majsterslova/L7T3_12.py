4ad76d5a7b064cba7e2262c179763be7da0e3b75
majsterslowa <morozoleksandr2018@gmail.com>
2024-03-21 20:07:53 +0100

    HW7T3

---
+++ hw/hw07/majsterslova/L7T3_12.py
@@ -0,0 +1,7 @@
+def correct_tail(body, tail):
+    if body[-1] == tail:
+        return True
+    else:
+        return False
+     #sub = body.substr(len(body)-len(tail.length)
+        #if sub = tai:
