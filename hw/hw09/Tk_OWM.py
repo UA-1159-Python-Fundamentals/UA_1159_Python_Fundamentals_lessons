b050517d3752c076c195ef45df172596b1b93baf
gawanyn <shcherbantaras07@gmail.com>
2024-03-22 01:00:17 +0200

    hw09_1

---
+++ hw/hw09/Tk_OWM.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+from tkinter import font
+from OWM import get_weather
+
+HEIGHT = 350
+WIDTH = 450
+
+def update_label():
+    weather_details = get_weather()
+    text = '\n'.join(weather_details)
+    label['text'] = text
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+root.title("Weather Application")
+canvas.pack()
+
+frame = tk.Frame(root, bg="deep sky blue", bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry_field = tk.Entry(frame, font=('Courier', 12))
+entry_field.place(relx=0, rely=0, relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, 
+                   text="Get Weather", 
+                   bg="gray", fg="white", 
+                   font=('Courier', 8), 
+                   command= update_label)
+button.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
+
+lower_frame = tk.Frame(root, bg='gold', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame, font=('Courier', 14), justify='left')
+label.place(relx=0, rely=0, relwidth=1, relheight=1)
+# label.config(text ='/n'.join(get_weather()))
+
+# label.config(text='\n'.join(weather_details))
+
+# def update_label():
+#     label.config(text='\n'.join(weather_details))
+
+# get_weather()
+# update_label()
+
+root.mainloop()
+
