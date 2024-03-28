acfcbedb9598347279dbf5c0b7fd0ea345519c31
gawanyn <shcherbantaras07@gmail.com>
2024-03-28 20:15:34 +0200

    hw11

---
+++ hw/hw11/gawanyn/hw11_coderunner1.py
@@ -0,0 +1,37 @@
+from random import choices
+from string import ascii_letters, digits
+
+def check(login):
+  
+    login = login.lower()
+    role, id_part = "", ""
+    separators = ["-id", "id", "-"]
+
+    for separator in separators:
+      parts = login.split(separator)
+      if len(parts) == 2:
+        role, id_part = parts
+        break
+
+    if role not in ("admin", "employee"):
+      raise ValueError(f"incorrect login '{login}'")
+
+    try:
+      int(id_part)  # Check if the id part is a valid integer
+    except ValueError:
+      raise ValueError(f"incorrect login '{login}'")
+
+    return True
+
+letters_and_digits = choices(ascii_letters, k=5)
+print(letters_and_digits)
+letters_and_digits.extend(choices(digits, k=5))
+
+incorect_login = ''.join(letters_and_digits)
+print(incorect_login)
+
+try:
+    if check(incorect_login):
+        print("checked successfully")
+except ValueError as e:
+    print(str(e) == f"incorrect login '{incorect_login}'")
