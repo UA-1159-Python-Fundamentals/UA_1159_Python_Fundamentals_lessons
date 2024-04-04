4f9e97c5db308643ad656ddd3e1146679739671a
gawanyn <shcherbantaras07@gmail.com>
2024-04-04 19:09:22 +0300

    added project

--- projects/gawanyn/calculator tkinter.py
+++ projects/gawanyn/calculator tkinter.py
@@ -50,7 +50,10 @@ def button_equal():
     if math == "multiplication":
         entry_field.insert(0, f_num * float(second_number))
     if math == "division":
-        entry_field.insert(0, f_num / float(second_number))
+        try:
+            entry_field.insert(0, f_num / float(second_number))
+        except ZeroDivisionError as er:
+            entry_field.insert(0, er)
 
 root = tk.Tk()
 root.title("Simple Calculator")
@@ -58,21 +61,22 @@ root.title("Simple Calculator")
 entry_field = tk.Entry(root, width=35, borderwidth=5)
 entry_field.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-button_1 = tk.Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-button_2 = tk.Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-button_3 = tk.Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-button_4 = tk.Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-button_5 = tk.Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-button_6 = tk.Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-button_7 = tk.Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-button_8 = tk.Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-button_9 = tk.Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button_0 = tk.Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_add = tk.Button(root, text="+", padx=39, pady=20, command=button_add)
-button_subtract = tk.Button(root, text="-", padx=41, pady=20, command=button_subtract)
-button_multiply = tk.Button(root, text="*", padx=41, pady=20, command=button_multiply)
-button_divide = tk.Button(root, text="/", padx=41, pady=20, command=button_divide)
-button_equal = tk.Button(root, text="=", padx=91, pady=20, command=button_equal)
+button_1 = tk.Button(root, text="1", padx=40, pady=20, overrelief="flat", command=lambda: button_click(1))
+button_2 = tk.Button(root, text="2", padx=40, pady=20, overrelief="flat", command=lambda: button_click(2))
+button_3 = tk.Button(root, text="3", padx=40, pady=20, overrelief="flat", command=lambda: button_click(3))
+button_4 = tk.Button(root, text="4", padx=40, pady=20, overrelief="flat", command=lambda: button_click(4))
+button_5 = tk.Button(root, text="5", padx=40, pady=20, overrelief="flat", command=lambda: button_click(5))
+button_6 = tk.Button(root, text="6", padx=40, pady=20, overrelief="flat", command=lambda: button_click(6))
+button_7 = tk.Button(root, text="7", padx=40, pady=20, overrelief="flat", command=lambda: button_click(7))
+button_8 = tk.Button(root, text="8", padx=40, pady=20, overrelief="flat", command=lambda: button_click(8))
+button_9 = tk.Button(root, text="9", padx=40, pady=20, overrelief="flat", command=lambda: button_click(9))
+button_0 = tk.Button(root, text="0", padx=40, pady=20, overrelief="flat", command=lambda: button_click(0))
+
+button_add = tk.Button(root, text="+", padx=40, pady=20, command=button_add)
+button_subtract = tk.Button(root, text="-", padx=40, pady=20, command=button_subtract)
+button_multiply = tk.Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_divide = tk.Button(root, text="/", padx=40, pady=20, command=button_divide)
+button_equal = tk.Button(root, text="=", padx=89, pady=20, command=button_equal)
 button_clear = tk.Button(root, text="Clear", padx=79, pady=20, command=button_clear)
 
 button_1.grid(row=3, column=0)
@@ -88,6 +92,7 @@ button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
+
 button_clear.grid(row=4, column=1, columnspan=2)
 
 button_add.grid(row=5, column=0)
